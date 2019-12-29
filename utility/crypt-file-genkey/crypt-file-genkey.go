7983b0219001a81283cdca2d93ea0defd10c2259
hangilc <hangil@chang.jp>
2019-12-29 23:55:05 +0900

    revised genkey

--- utility/crypt-file-genkey/crypt-file-genkey.go
+++ utility/crypt-file-genkey/crypt-file-genkey.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/hex"
+	"flag"
 	"fmt"
+	"os"
 )
 
 var keybytes = 16
+var outfile = flag.String("o", "", "output file")
 
 func main() {
+	flag.Parse()
 	key := make([]byte, keybytes)
 	_, err := rand.Read(key)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%x\n", key)
+	rep := hex.EncodeToString(key)
+	if *outfile != "" {
+		f, err := os.OpenFile(*outfile, os.O_CREATE|os.O_EXCL, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		_, err = f.WriteString(rep)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Printf("%s\n", rep)
+	}
 }
