33450d4e7a3b0084ea54c90582e615450c81ccb7
hangilc <hangil@chang.jp>
2019-12-30 17:04:42 +0900

    clean upped internal/encrypt.go

--- internal/encrypt.go
+++ internal/encrypt.go
@@ -1,24 +1,22 @@
 package internal
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
 )
 
-func Encrypt(ver int, key []byte, nonce []byte, plain []byte) ([]byte, error) {
-	head, err := CreateHeader(ver, nonce)
-	if err != nil {
-		return nil, err
-	}
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-	aead, err := cipher.NewGCM(block)
-	ciphertext := aead.Seal(plain[:0], nonce, plain, nil)
-	return append(head, ciphertext...), nil
-}
+// func Encrypt(ver int, key []byte, nonce []byte, plain []byte) ([]byte, error) {
+// 	head, err := CreateHeader(ver, nonce)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	block, err := aes.NewCipher(key)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	aead, err := cipher.NewGCM(block)
+// 	ciphertext := aead.Seal(plain[:0], nonce, plain, nil)
+// 	return append(head, ciphertext...), nil
+// }
 
 func CreateNonce() ([]byte, error) {
 	nonce := make([]byte, 12)
